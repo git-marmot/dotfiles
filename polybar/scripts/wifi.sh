693af0e8da58cd86205ef340bffa4f0c9d192840
git-marmot <119555616+git-marmot@users.noreply.github.com>
2023-08-10 04:17:01 +0200

    Updated relative path

--- polybar/scripts/wifi.sh
+++ polybar/scripts/wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ping=$(ping -c 1 8.8.8.8 2>&1)
-vpn=$(/home/drunkenmarmot/.config/polybar/scripts/mullvad.sh)
+vpn=$($HOME/.config/polybar/scripts/mullvad.sh)
 
 if [[ $ping =~ "time=" ]]; then
 	line=$(echo "$ping" |grep -o 'time=[^ ]*' | cut -d '=' -f 2)
